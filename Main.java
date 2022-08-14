222e647a73aa7872094e1a645894a4f158d65acc
ahajiyev628 <66174474+ahajiyev628@users.noreply.github.com>
2022-08-14 23:36:51 +0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,20 +10,20 @@ public class Main {
 
         mother.setName("Tito");
         mother.setSurname("Karleone");
-        mother.setYear(1960);
+        /*mother.setYear(1960);
         mother.setIq(60);
         mother.setFather(new Human("Allahverdi", "Hajiyev"));
         mother.setMother(new Human("Allahverdi", "Hajiyev"));
-        mother.setSchedule(new String[][]{{String.valueOf(DayOfWeek.Sunday)}, {Human.tasks[0]}});
+        mother.setSchedule(new String[]{String.valueOf(DayOfWeek.Sunday), Human.tasks[0]});*/
 
 
         father.setName("Vito");
         father.setSurname("Karleone");
-        father.setYear(1970);
+        /*father.setYear(1970);
         father.setIq(70);
         father.setFather(new Human("Allahverdi", "Hajiyev"));
         father.setMother(new Human("Allahverdi", "Hajiyev"));
-        father.setSchedule(new String[][]{{String.valueOf(DayOfWeek.Thursday)}, {Human.tasks[4]}});
+        father.setSchedule(new String[]{String.valueOf(DayOfWeek.Thursday), Human.tasks[4]});*/
 
         child.setName("Michael");
         child.setSurname("Karleone");
@@ -31,7 +31,7 @@ public class Main {
         child.setIq(50);
         child.setFather(new Human(father.getName(), father.getSurname()));
         child.setMother(new Human(mother.getName(), mother.getSurname()));
-        child.setSchedule(new String[][]{{String.valueOf(DayOfWeek.Saturday)}, {Human.tasks[6]}});
+        child.setSchedule(new String[]{String.valueOf(DayOfWeek.Saturday), Human.tasks[6]});
 
         ArrayList<Human> childs = family.addChild(child);
         for(Human ch : childs) System.out.println(ch.toString());
@@ -39,18 +39,16 @@ public class Main {
         DomesticCat dc = new DomesticCat(AnimalSpecies.DomesticCat, "wimel", 10, 50, new String[]{"eat", "play"});
         System.out.println(dc.describePet());
 
-        RoboCat rc = new RoboCat(AnimalSpecies.ROBOCAT, "wimel", 10, 50, new String[]{"eat", "play"});
+        RoboCat rc = new RoboCat(AnimalSpecies.ROBOCAT, "wimel", 10, 49, new String[]{"eat", "play"});
         System.out.println(rc.describePet());
 
-        Fish f = new Fish(AnimalSpecies.FISH, "wimel", 10, 50, new String[]{"eat", "play"});
+        Fish f = new Fish(AnimalSpecies.FISH, "wimel", 10, 70, new String[]{"eat", "play"});
         System.out.println(f.describePet());
 
-        Dog dg = new Dog(AnimalSpecies.DOG, "wimel", 10, 50, new String[]{"eat", "play"});
+        Dog dg = new Dog(AnimalSpecies.DOG, "wimel", 10, 51, new String[]{"eat", "play"});
         System.out.println(dg.describePet());
 
         System.out.print("Count of members in the family: " + family.countFamily());
 
     }
 }
-
-
